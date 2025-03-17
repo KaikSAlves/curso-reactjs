9e74868e3eb21748a2ed9797e7b7ddd22002568b
KaikSAlves <souzaalveskaik@gmail.com>
2025-03-17 10:55:23 -0300

    criado pagina contato e referencia ao diretorio PUBLIC

---
+++ src/pages/contato/index.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+export default function Contato(){
+
+    return (
+        <div className='pagina-contato'>
+            <h1>
+                Contato
+            </h1>
+            <img className = 'icone' src ='/assets/images/contato.png'/>
+        </div>
+    )
+}
