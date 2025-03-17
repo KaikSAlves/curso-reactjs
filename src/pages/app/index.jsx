2aaef47f841767d32032e9a908091448609f7210
KaikSAlves <souzaalveskaik@gmail.com>
2025-03-17 12:25:02 -0300

    criado estilizacao global

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,17 +1,38 @@
-import './index.scss';
+import "./index.scss";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="pagina-app">
-      <h1 className='titulo's>Estudos de React Js</h1>
+    <div className="pagina-app pagina">
+      <header className="cabecalho">
+        <h1 className="titulo" s>
+          Estudos de React Js
+          <i className="fa fa-heart"></i>
+        </h1>
+      </header>
 
-      <ul>
-        <li>
-          <Link to='/contato'>Ir para contato</Link>
-        </li>
-      </ul>
+      <section className="secao">
+        <h1>Estudando ReactJS</h1>
+
+        <input type="text" placeholder="Digite aqui"/>
+        <br/><br/>
+
+        <select>
+          <option>Item 1</option>
+          <option>Item 2</option>
+        </select>
+
+        <br/><br/>
+        <button>Clique aqui</button>
+        <br/><br/>
+        
+        <ul>
+          <li>
+            <Link to="/contato">Ir para contato</Link>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -1,15 +1,19 @@
-import { Link } from 'react-router-dom'
-import './index.scss'
+import { Link } from "react-router-dom";
+import "./index.scss";
 
-
-
-export default function Contato(){
-
-    return (
-        <div className='pagina-contato'>
-            <Link to='/'><i className='fa fa-arrow-left voltar'></i></Link>
-            <h1 className='titulo'>Contato</h1>
-            <img className = 'icone' src ='/assets/images/contato.png'/>
-        </div>
-    )
+export default function Contato() {
+  return (
+    <div className="pagina-contato pagina">
+      <header className="cabecalho">
+        <Link to="/">
+          <i className="fa fa-arrow-left voltar"></i>
+        </Link>
+        <h1 className="titulo">Contato</h1>
+      </header>
+      <section className="secao">
+        <h1>Entre em contato!</h1>
+        <img className="icone" src="/assets/images/contato.png" />
+      </section>
+    </div>
+  );
 }
