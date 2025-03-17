4b6d8bf4fe91de0f74646f985ca6f5afb860c3e1
KaikSAlves <souzaalveskaik@gmail.com>
2025-03-17 11:24:58 -0300

    Navegavao, pagina nao encontrada

---
+++ src/pages/notFound/index.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+export default function NotFound(){
+
+    return (
+        <div className='page-not-found'>
+            <div>
+            <h1 className='title'>Page not found</h1>
+            <img src="/assets/images/not-found.png" className='img-not-found' alt="" />
+            </div>
+        </div>
+    )
+}
---
+++ src/router.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./pages/app";
+import Contato from "./pages/contato";
+import NotFound from './pages/notFound';
+
+export default function Navegecao() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/contato" element={<Contato />} />
+
+        <Route path="*" element = { <NotFound/> }/>
+      </Routes>
+    </BrowserRouter>
+  );
+
+}
